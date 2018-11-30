f79bd7d290fe1316ccd192b0051ccaa5a6eacff5
zhangxiao-ms <zhangxiao-ms@360.cn>
2018-11-30 11:36:16 +0800

    add android o andoird pie buid bash

---
+++ tests/build-pie.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Test script file that maps itself into a docker container and runs
+#
+# Example invocation:
+#
+# $ AOSP_VOL=$PWD/build ./build-nougat.sh
+#
+set -ex
+
+cpus=$(grep ^processor /proc/cpuinfo | wc -l)
+
+repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-9.0.0_r18
+
+# Use default sync '-j' value embedded in manifest file to be polite
+repo sync
+
+prebuilts/misc/linux-x86/ccache/ccache -M 10G
+
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
+
+source build/envsetup.sh
+lunch aosp_arm-eng
+make -j $cpus
+
